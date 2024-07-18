49c4cb58c1a6435e7a52e6a0de9e695586d1710b
Diogen24 <166066710+Diogen24@users.noreply.github.com>
2024-07-19 00:15:07 +0300

    Add files via upload

---
+++ echo-bot.py
@@ -0,0 +1,11 @@
+import telebot
+from bot_token import TOKEN
+
+bot=telebot.TeleBot(TOKEN)
+
+@bot.message_handler(content_types=['text'])
+def echo(message):
+    bot.send_message(message.chat.id, message.text*10)
+    
+if __name__=='__main__':
+    bot.infinity_polling()
